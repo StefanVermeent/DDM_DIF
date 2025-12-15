7982bf1ff30c0173897e695eb97a959100e32bdf
Stefan Vermeent <p.c.s.vermeent@gmail.com>
2025-12-15 14:57:44 +0100

    Install dependencies using Groundhog

---
+++ 2_scripts/dependencies.R
@@ -0,0 +1,34 @@
+#install.packages('groundhog')
+library(groundhog)
+
+# Only run the commented out code below if the `groundhog/` folder needs to be initiated again 
+
+meta.groundhog("2025-10-01")
+#groundhog::set.groundhog.folder(file.path(getwd(), 'groundhog'))
+
+# Dependencies for this project
+pkgs <- 
+  c(
+    "tidyverse",
+    "haven",
+    "OpenMx",
+    "lavaan",
+    "sjlabelled",
+    "glue",
+    "RWiener",
+    "flextable",
+    "english",
+    "ggsci",
+    "rstan",
+    "runjags",
+    "finalfit",
+    "openxlsx"
+  )
+
+
+# The date of which we want to install and load the package versions
+date <- "2025-10-01"
+
+# Using the groundhog package, we install and load the project dependencies
+groundhog.library(pkg = pkgs, date = date)
+
